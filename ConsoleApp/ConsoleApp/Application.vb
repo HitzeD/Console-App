f779861b8f0402e0b3e9408ce12dc6352fedb1af
HitzeD <d-hitze@comcast.net>
2019-09-25 20:14:45 -0400

    finished file

--- ConsoleApp/ConsoleApp/Application.vb
+++ ConsoleApp/ConsoleApp/Application.vb
@@ -2,9 +2,57 @@
 Imports ConsoleApp.Models
 
 Public Class Application
+
 	Public Shared Sub Main()
-		Dim cust1 As New BankAccount("Dakotah", "Hitze", 200.00)
+    
+		Dim cust1 As BankAccount
+        Dim quit AS Boolean
+        Dim fname As String
+        Dim lname As String
+        Dim initial As String
         
-        Console.WriteLine(cust1.GetName())
+        Console.WriteLine("What is your First Name: ")
+        fname = Console.ReadLine()
+        Console.WriteLine("What is your Last Name: ")
+        lname = Console.ReadLine()
+        Console.WriteLIne("How much is the initial deposit: ")
+        initial = Console.ReadLine()
+        
+        cust1 = New BankAccount(fname, lname, CDbl(initial))
+        
+        quit = True
+        while quit
+        
+            Dim temp As String
+            Dim deptemp As String
+            'Dim lower As String
+            Console.WriteLine($"Hello {cust1.GetName()}, What would you like to do?: (Q)uit, (D)eposit, (P)urchase, (A)mount")
+            temp = Console.ReadLine()
+            'lower = LCase(temp)
+            
+            If (temp = "q") Then
+            
+                quit = False
+                
+            ElseIf (temp = "d") Then
+            
+                Console.WriteLine("Deposit Amount: ")
+                deptemp = Console.ReadLine()
+                cust1.Deposit(CDbl(deptemp))
+                
+            ElseIf (temp = "p") Then
+            
+                Console.WriteLine("Purchase Amount: ")
+                deptemp = Console.ReadLine()
+                cust1.Purchase(CDbl(deptemp))
+                
+            ElseIf (temp = "a") Then
+            
+                cust1.GetAmount()
+                
+            End If
+        End while
+        
+        
 	End Sub
 End Class
--- ConsoleApp/ConsoleApp/Models/BankAccount.vb
+++ ConsoleApp/ConsoleApp/Models/BankAccount.vb
@@ -1,7 +1,7 @@
 ﻿
 Option Explicit On
 Option Strict On
-
+Imports System
 
 
 Namespace Models
@@ -35,12 +35,19 @@ Namespace Models
         
         Public Function Purchase(ByVal purAmount As Double) As String
             amount -= purAmount
+            Console.WriteLine($"You spent {purAmount}, You have {amount} left!")
             Return $"You spent {purAmount}, You have {amount} left!"
         End Function
         
         Public Function Deposit(ByVal depAmount As Double) As String
             amount += depAmount
+            Console.WriteLine($"You deposited {depAmount}, You now have {amount}!")
             Return $"You deposited {depAmount}, You now have {amount}!"
+        End Function
+        
+        Public Function GetAmount() As Double
+            Console.WriteLine($"You have {amount}!")
+            Return amount
         End Function
         
 	End Class
